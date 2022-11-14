64fd82affc8a8f1a03015551e79edc4cb76655a5
Victor_2103 <vitcheffvi@cy-tech.fr>
2022-11-14 16:47:12 +0100

    add etirement homgeneité

--- Translation/test.py
+++ Translation/test.py
@@ -2,23 +2,26 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 
-mario=Image.new("RGB",(8,8),"white")  
+mario = Image.new("RGB", (8, 8), "white")
 
-test=Image.open("images/test.png")
+test = Image.open("../images/test.png")
 print(test.size[0])
-test.show()
+# test.show()
 
-translation=Image.new("RGB",(test.size[0]+200,test.size[1]),"white")
-rouge=(255,0,0)
+var = 3
+
+translation = Image.new("RGB", (var*test.size[0], var*test.size[1]), "white")
+rouge = (255, 0, 0)
+
+print(test.getpixel((5, 5)))
+
 
 for i in range(test.size[0]):
     for j in range(test.size[1]):
-        translation.putpixel((i+200,j),test.getpixel((i,j)))
+        translation.putpixel((var*i, j), test.getpixel((i, j)))
 
 
 translation.show()
-
-
 
 
 '''
