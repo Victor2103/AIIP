609ca3d3824a73a0b8c38a0bf81fbd92dfe81417
Victor_2103 <vitcheffvi@cy-tech.fr>
2022-11-28 22:56:32 +0100

    add cisaillement

--- Translation/homothetie.py
+++ Translation/homothetie.py
@@ -32,14 +32,3 @@ translation.show()
 test.show()
 
 
-'''
-# Create the red, blue and green color
-R = [255, 0, 0]
-G = [0, 255, 0]
-B = [0, 0, 255]
-# Create the RGB first table
-RGB = np.array([[G, B, R], [B, R, G], [R, G, B]], dtype=np.uint8)
-# Disp the RGB first table
-plt.imshow(RGB)
-plt.show()
-'''
