7a1d9e6e0cf1a40664ecc421071c94dfca1ae15c
Victor_2103 <vitcheffvi@cy-tech.fr>
2022-11-28 23:15:07 +0100

    push translation

--- Resizing/resizing.py
+++ Resizing/resizing.py
@@ -1,6 +1,4 @@
-import numpy as np
 from PIL import Image
-import matplotlib.pyplot as plt
 
 test = Image.open("../images/test.png")
 # test.show()
---
+++ Translation/translation.py
@@ -0,0 +1,25 @@
+import numpy as np
+from PIL import Image
+
+
+test = Image.open("../images/test.png")
+# test.show()
+
+print("Entrez la translation horizontale puis verticale !")
+droite = float(input("Horizontale : "))
+gauche = float(input("Verticale : "))
+
+
+im_array = np.array(test)
+
+
+translation=Image.new("RGBA", (im_array.shape[0]+int(gauche), im_array.shape[1]+int(droite)), "white")
+translation_arr=np.array(translation)
+for i in range(im_array.shape[0]):
+    for j in range(im_array.shape[1]):
+        if (i+gauche<translation_arr.shape[0]) and (j+droite<translation_arr.shape[1]):
+            translation_arr[i+int(gauche),j+int(droite)]=im_array[i,j]
+
+translation=Image.fromarray(translation_arr)
+
+translation.show()
